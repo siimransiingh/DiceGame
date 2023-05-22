5a5366ef75835afdffdd56d0346d3edee86ed39f
siimransiingh <72371901+siimransiingh@users.noreply.github.com>
2023-05-22 20:47:39 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,24 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6)+1; 
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randonImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randonImageSource); 
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1; 
+
+var randonImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randonImageSource2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 wins!"
+}
+else if (randomNumber1<randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 2 wins!"
+}
+
